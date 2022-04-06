746c1c7a369d1e561cc6ef6f61e588aecfaced62
HassanObeid <hassan.obeid.ho@hotmail.com>
2022-04-06 19:49:32 +0200

    Add benefit section to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import BenefitSection from './components/BenefitSection';
 import Hero from './components/Hero'
 export default function App() {
   return (
     <div className="App">
       <Hero/>
+      <BenefitSection/>
     </div>
   );
 }
