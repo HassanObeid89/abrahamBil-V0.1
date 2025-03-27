0ee7ec08c0197eab48246f1e70db9013da8df01c
Hasse Obeid <hassan.obeid.ho@hotmail.com>
2025-03-27 18:39:45 +0100

    Added hash tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,19 @@ function Footer() {
   return (
     <div className="footer">
       <Segment className="segment"  style={{ padding: "2em 0em", backgroundColor:"rgb(233, 227, 227)" }}>
+        <Container textAlign={"center"} text style={{ marginBottom: "1em" }}>
+          <p>
+          © viköperdefektbil.se
+          </p>
+        </Container>
         <Container textAlign={"center"} text>
           <p>
-          © bilarköpes.se
+            #defekta bilar #trasiga bilar #skrotbilar #skrotbil #skrotbilar #skrotbil
+            #vi köper defekta bilar #vi köper skrotbilar #vi köper trasiga bilar
+            #vi köper bilar
+          </p>
+          <p>
+            #defekta bilar köpes #skrotbilar köpes #trasiga bilar köpes
           </p>
         </Container>
       </Segment>
